4133a31a426ed79982960595c174e7ba30d9306d
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-10-07 13:23:34 +0300

    try react

---
+++ src/Greeting.jsx
@@ -0,0 +1,6 @@
+function Greeting() {
+  const avatar = "https://i.imgur.com/7vQD0fPs.jpg";
+  const description = "Gregorio Y. Zara";
+  return <img className="avatar" src={avatar} description={description} />;
+}
+export default Greeting;
