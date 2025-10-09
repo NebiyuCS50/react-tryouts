d9b71458b29b9e104f0b21284a16dd16fbea694e
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-10-09 20:15:07 +0300

    try react

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Greeting from "./Greeting";
-
+import Conditional from "./Conditional";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Greeting />
+    {/* <App /> */}
+    <Conditional />
   </StrictMode>
 );
