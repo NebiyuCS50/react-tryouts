89a8cc5b91238bf51e8da262982b0f35a8234ad2
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-10-09 21:01:30 +0300

    try react

--- src/Conditional.jsx
+++ src/Conditional.jsx
@@ -1,39 +1,23 @@
-import "./App.css";
-
-function Button({
-  text = "Click me",
-  color = "blue",
-  fontSize = 12,
-  marginTop = 20,
-  handleButton,
-}) {
-  const buttonStyle = {
-    color: color,
-    fontSize: fontSize + "px",
-    marginTop: marginTop + "px",
-  };
+import { useState } from "react";
+import "./Conditional.css";
+function Counter() {
+  const [count, setCount] = useState(0);
+  function increment() {
+    setCount(count + 1);
+  }
+  function decrement() {
+    setCount(count - 1);
+  }
+  function reset() {
+    setCount(0);
+  }
   return (
-    <button onClick={handleButton} style={buttonStyle}>
-      {text}
-    </button>
-  );
-}
-
-function ButtonApp() {
-  const handleButtonClick = (url) => {
-    window.location.href =
-      url == null || url == undefined ? "https://www.google.com" : url;
-  };
-  return (
-    <div className="App">
-      <Button
-        text="Go to Google"
-        color="red"
-        fontSize={20}
-        marginTop={30}
-        handleButton={handleButtonClick()}
-      />
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
-export default ButtonApp;
+export default Counter;
