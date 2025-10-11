60f79b32de840c201644b1511c500d60617afafb
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-10-11 15:37:11 +0300

    try react

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import "./index.css";
 import App from "./App";
 import Conditional from "./Conditional";
 import Person from "./UseState";
+import Try from "./Try";
+import SharedState from "./SharedState";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <SharedState />
     {/* <App /> */}
     {/* <Conditional /> */}
-    <Person />
+    {/* <Person /> */}
+    {/* <Try /> */}
   </StrictMode>
 );
