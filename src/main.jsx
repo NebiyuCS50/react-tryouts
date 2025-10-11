2ab6e15bd9995589b14c1be7e7f190c8dbf3cb43
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-10-11 10:09:12 +0300

    try react

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Conditional from "./Conditional";
+import Person from "./UseState";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <Conditional />
+    {/* <Conditional /> */}
+    <Person />
   </StrictMode>
 );
