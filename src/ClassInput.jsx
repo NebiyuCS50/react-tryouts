d87fb2a8f9dc089146a4af127a97502726673b3a
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-10-27 16:49:09 +0300

    add ClassInput component for managing todo list

---
+++ src/ClassInput.jsx
@@ -0,0 +1,66 @@
+import { Component } from "react";
+
+class ClassInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [],
+      inputVal: "",
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
+  }
+
+  handleInputChange(e) {
+    this.setState((state) => ({
+      ...state,
+      inputVal: e.target.value,
+    }));
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState((state) => ({
+      todos: state.todos.concat(state.inputVal),
+      inputVal: "",
+    }));
+  }
+  deleteTodo(todoToDelete) {
+    this.setState((state) => ({
+      todos: state.todos.filter((todo) => todo !== todoToDelete),
+    }));
+  }
+
+  render() {
+    return (
+      <section>
+        <h3>{this.props.name}</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="task-entry">Enter a task: </label>
+          <input
+            type="text"
+            id="task-entry"
+            name="task-entry"
+            value={this.state.inputVal}
+            onChange={this.handleInputChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <h4>All the tasks!</h4>
+        <ul>
+          {this.state.todos.map((todo) => (
+            <li key={todo}>
+              {todo}
+              <button onClick={() => this.deleteTodo(todo)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+}
+
+export default ClassInput;
