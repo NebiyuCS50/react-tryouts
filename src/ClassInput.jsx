ae7f0ee447ab1e7dd889e35db3bed61b0c9a2364
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-10-27 18:19:36 +0300

    add ClassInputCounter component and implement edit functionality for todos

--- src/ClassInput.jsx
+++ src/ClassInput.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import ClassInputCounter from "./ClassInputCounter";
 
 class ClassInput extends Component {
   constructor(props) {
@@ -7,30 +8,61 @@ class ClassInput extends Component {
     this.state = {
       todos: [],
       inputVal: "",
+      editId: null,
+      editText: "",
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.deleteTodo = this.deleteTodo.bind(this);
+    this.startEdit = this.startEdit.bind(this);
+    this.handleInputChangeEdit = this.handleInputChangeEdit.bind(this);
+    this.saveEdit = this.saveEdit.bind(this);
   }
 
   handleInputChange(e) {
-    this.setState((state) => ({
-      ...state,
-      inputVal: e.target.value,
-    }));
+    this.setState({ inputVal: e.target.value });
   }
 
   handleSubmit(e) {
     e.preventDefault();
+    const id = crypto.randomUUID();
+    const text = this.state.inputVal.trim();
+    if (!text) {
+      alert("Input cannot be empty or whitespace only");
+      return;
+    }
     this.setState((state) => ({
-      todos: state.todos.concat(state.inputVal),
+      todos: state.todos.concat({ id, text: state.inputVal }),
       inputVal: "",
     }));
   }
   deleteTodo(todoToDelete) {
     this.setState((state) => ({
-      todos: state.todos.filter((todo) => todo !== todoToDelete),
+      todos: state.todos.filter((todo) => todo.id !== todoToDelete.id),
+    }));
+  }
+  startEdit(todo) {
+    this.setState((state) => ({
+      ...state,
+      editId: todo.id,
+      editText: todo.text,
+    }));
+  }
+  handleInputChangeEdit(e) {
+    this.setState((state) => ({
+      ...state,
+      editText: e.target.value,
+    }));
+  }
+  saveEdit(e) {
+    e.preventDefault();
+    this.setState((state) => ({
+      todos: state.todos.map((todo) =>
+        todo.id === state.editId ? { ...todo, text: state.editText } : todo
+      ),
+      editId: null,
+      editText: "",
     }));
   }
 
@@ -52,12 +84,25 @@ class ClassInput extends Component {
         <h4>All the tasks!</h4>
         <ul>
           {this.state.todos.map((todo) => (
-            <li key={todo}>
-              {todo}
+            <li key={todo.id}>
+              {todo.text}{" "}
+              {this.state.editId === todo.id ? (
+                <>
+                  <input
+                    type="text"
+                    value={this.state.editText}
+                    onChange={this.handleInputChangeEdit}
+                  />
+                  <button onClick={this.saveEdit}>Resubmit</button>
+                </>
+              ) : (
+                <button onClick={() => this.startEdit(todo)}>Edit</button>
+              )}
               <button onClick={() => this.deleteTodo(todo)}>Delete</button>
             </li>
           ))}
         </ul>
+        <ClassInputCounter count={this.state.todos.length} />
       </section>
     );
   }
